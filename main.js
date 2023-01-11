0de59f3566649b3aba768c37a7701c168c8460d0
Julian Baranowski <julian.paul.baranowski@gmail.com>
2023-01-10 19:59:14 -0700

    second commit

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.listen(4004, () => console.log('Listining on port 4004'))
+
+
 function buttonClick(params) {
     
 }
 
-const residentButton = document.querySelector("#resident);
+//const residentButton = document.querySelector("#resident);
